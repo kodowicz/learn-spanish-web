09cfca6ad1226f8f782e172c98bb9e3e322cbf96
Anna Kotowicz <anna.kotowicz@monterail.com>
2022-07-09 15:25:56 +0200

    [LS-02] Setup Eslint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import React from "react";
+
 function App() {
-  return (
-    <h1>Learn Spanish</h1>
-  )
+  return <h1>Learn Spanish</h1>;
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootNode = document.getElementById("root") as Element;
+
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
