d36d488c89c225fe98b8b5af07003e6d7fad4f2a
Szymon Ograbek <szymon.ograbek@monterail.com>
2022-12-21 21:30:25 +0100

    chore: init and setup
    Init the app with Vite + React template
    Add react-query, react-location, prettier, eslint, tailwindcss and framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  ReactLocation,
+  Router,
+  Outlet
+} from '@tanstack/react-location';
+import { motion } from 'framer-motion';
+
+import './index.css';
+
+const queryClient = new QueryClient();
+
+const location = new ReactLocation();
 
 function App() {
-  return <h1>Learn Spanish</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router location={location} routes={[{path: '/', element: 'Home!'}]}>
+        <motion.div
+          className="m-40 h-32 w-32 bg-purple-500"
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 180, 180, 0],
+            borderRadius: ['0%', '0%', '50%', '50%', '0%']
+          }}
+          transition={{
+            duration: 2,
+            ease: 'easeInOut',
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1
+          }}
+        />
+        <Outlet />
+      </Router>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const rootNode = document.getElementById("root") as Element;
-
-ReactDOM.createRoot(rootNode).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
