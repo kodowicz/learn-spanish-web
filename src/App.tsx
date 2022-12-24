d38f0b2fa1e699c39d161e8b58b95d5e85b2389e
Szymon Ograbek <szymon.ograbek@monterail.com>
2022-12-24 15:16:52 +0100

    chore: add lefthook and additional eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {
-  ReactLocation,
-  Router,
-  Outlet
-} from '@tanstack/react-location';
-import { motion } from 'framer-motion';
+import { ReactLocation, Router, Outlet } from '@tanstack/react-location';
 
 import './index.css';
 
@@ -15,22 +10,7 @@ const location = new ReactLocation();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router location={location} routes={[{path: '/', element: 'Home!'}]}>
-        <motion.div
-          className="m-40 h-32 w-32 bg-purple-500"
-          animate={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 180, 180, 0],
-            borderRadius: ['0%', '0%', '50%', '50%', '0%']
-          }}
-          transition={{
-            duration: 2,
-            ease: 'easeInOut',
-            times: [0, 0.2, 0.5, 0.8, 1],
-            repeat: Infinity,
-            repeatDelay: 1
-          }}
-        />
+      <Router location={location} routes={[{ path: '/', element: 'Home!' }]}>
         <Outlet />
       </Router>
     </QueryClientProvider>
