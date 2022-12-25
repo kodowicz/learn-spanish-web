b5b351e8f55691d531c17a3832629c7af1a4e3dc
Szymon Ograbek <szymon.ograbek2@gmail.com>
2022-12-25 19:55:19 +0100

    chore: init and setup (#7)
    * chore: init and setup
    
    Init the app with Vite + React template
    Add react-query, react-location, prettier, eslint, tailwindcss and framer-motion
    Add lefthook and additional eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactLocation, Router, Outlet } from '@tanstack/react-location';
+
+import './index.css';
+
+const queryClient = new QueryClient();
+
+const location = new ReactLocation();
 
 function App() {
-  return <h1>Learn Spanish</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router location={location} routes={[{ path: '/', element: 'Home!' }]}>
+        <Outlet />
+      </Router>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
