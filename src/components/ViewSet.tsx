a67644916d58467cb13047830770e903a655f6af
Anna Kotowicz <anna.kotowicz@monterail.com>
2023-04-09 22:26:00 +0200

    chore: fix after eslint changes

--- src/components/ViewSet.tsx
+++ src/components/ViewSet.tsx
@@ -1,14 +1,16 @@
 interface ViewSetProps {
-  title: string,
-  author: string,
-  termsCount: number
+  title: string;
+  author: string;
+  termsCount: number;
 }
 
 export function ViewSet({ title, author, termsCount }: ViewSetProps) {
   return (
     <div>
       <h2>{title}</h2>
-      <p>{author} | {termsCount}</p>
+      <p>
+        {author} | {termsCount}
+      </p>
     </div>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -50,7 +50,7 @@ export const router = createBrowserRouter([
       {
         path: '/sets/:id',
         lazy: ViewSet,
-      }
+      },
     ],
   },
 ]);
