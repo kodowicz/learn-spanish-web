7ad150787b54f04d4fe4c759153a0e606a1471b0
Anna Kotowicz <anna.kotowicz@monterail.com>
2023-04-09 21:12:45 +0200

    assign default value for useSet

--- src/queries/useSet.ts
+++ src/queries/useSet.ts
@@ -9,8 +9,8 @@ function fetchSet(id: string): Promise<Set> {
   return axios.get(`/api/v1/study_sets/${id}`);
 }
 
-export function useSet(id: string | undefined) {
-  return useQuery(queryKey(id ?? ''), () => fetchSet(id ?? ''), {
+export function useSet(id: string | undefined = '') {
+  return useQuery(queryKey(id), () => fetchSet(id), {
     enabled: !!id,
     retry: false,
   });
