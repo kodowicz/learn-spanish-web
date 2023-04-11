516d9df1662cc01b71dbd4ff2fad0995e8e9dc4f
Anna Kotowicz <anna.julia.kotowicz@gmail.com>
2023-04-11 12:57:53 +0200

    chore: fix after Eslint changes (#16)
    chore: fix after eslint changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const axios = Axios.create({
   baseURL: API_URL,
   headers: {
-    'Accept': 'application/json',
+    Accept: 'application/json',
     'Content-Type': 'application/json',
   },
   withCredentials: true,
