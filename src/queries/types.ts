00f22750e0dca72b71a1fff64741220279c80666
Anna Kotowicz <anna.kotowicz@monterail.com>
2023-04-12 13:56:56 +0200

    register and login panel

--- src/queries/types.ts
+++ src/queries/types.ts
@@ -16,3 +16,14 @@ export interface Term {
   english: string;
   spanish: string;
 }
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCredentials {
+  email: string;
+  username: string;
+  password: string;
+}
